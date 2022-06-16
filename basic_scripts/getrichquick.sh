281ec980b101c2b218052766da0512a26fbcebd5
Andrey Burtasov <a.burtasov@yarus.ru>
2022-06-17 00:58:36 +0300

    fix getrich

--- basic_scripts/getrichquick.sh
+++ basic_scripts/getrichquick.sh
@@ -17,10 +17,15 @@ echo "Hello $name, you are $age years old."
 sleep 2
 
 echo "Calculating"
+sleep 1
 echo "..........."
+sleep 1
 echo "**........."
+sleep 1
 echo "****......."
+sleep 1
 echo "******....."
+sleep 1
 echo "***********"
 
 getrich=$((( $RANDOM % 15 ) + $age ))
